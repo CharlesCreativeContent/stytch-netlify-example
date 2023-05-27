02dfaff245cbed5194775ddf564edf6dcff9c827
Shawn Charles <62077627+CharlesCreativeContent@users.noreply.github.com>
2023-05-26 22:17:56 -0400

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,7 +5,7 @@ import { createStytchUIClient } from "@stytch/nextjs/ui";
 
 // We initialize the Stytch client using our project's public token which can be found in the Stytch dashboard
 const stytch = createStytchUIClient(
-  process.env.NEXT_PUBLIC_STYTCH_PUBLIC_TOKEN,
+  process.env.STYTCH_PUBLIC_TOKEN,
 );
 
 export default function App({ Component, pageProps }) {
